cf064f2a578af8aa4f6589669c83131424b32109
Thomas Harrison <twh2898@vt.edu>
2021-07-30 15:03:48 -0400

    Made library header only, added documentation to dispatch.hpp

--- include/dispatch.hpp
+++ include/dispatch.hpp
@@ -8,8 +8,17 @@
 #include <thread>
 #include <vector>
 
+/**
+ * An Apple style Task Dispatch Queue.
+ */
 class DispatchQueue {
 public:
+    /**
+     * Dispatch queue type. Serial executes tasks sequentially in a single
+     * thread while concurrent executes up to n tasks at once where n is
+     * the number of cpu cores as returned from
+     * `std::thread::hardware_concurrency()`.
+     */
     enum QueueType { Serial, Concurrent };
 
 private:
@@ -21,16 +30,81 @@ private:
     std::atomic<bool> running;
     std::queue<Fn> taskQueue;
 
-    void worker(size_t id);
+    /**
+     * Worker thread method. This method takes a task from the queue an executes
+     * it.
+     *
+     * @param id the worker id
+     */
+    void worker(size_t id) {
+        while (running) {
+            std::unique_lock lk(m);
+            cv.wait(lk, [&]() {
+                return !running || !taskQueue.empty();
+            });
+
+            if (!running)
+                break;
+
+            auto task = taskQueue.front();
+            taskQueue.pop();
+            lk.unlock();
+            task();
+        }
+    }
 
 public:
-    DispatchQueue(QueueType type);
+    /**
+     * Create a new `DispatchQueue` with queue `type`.
+     * @param type the queue type
+     */
+    DispatchQueue(QueueType type) {
+        workers.emplace_back(&DispatchQueue::worker, this, 0);
+        if (type == Concurrent) {
+            size_t n = std::thread::hardware_concurrency();
+            for (size_t i = 1; i < n; i++) {
+                workers.emplace_back(&DispatchQueue::worker, this, i);
+            }
+        }
+    }
 
-    bool working() const;
+    /**
+     * Check if there are tasks waiting for execution.
+     */
+    bool working() const {
+        std::unique_lock lk(m);
+        return !taskQueue.empty();
+    }
 
-    void stop();
+    /**
+     * Wait for all currently executing tasks to complete and skip any tasks
+     * that have not yet been started.
+     */
+    void stop() {
+        running = false;
+        cv.notify_all();
+        for (auto & worker : workers) {
+            worker.join();
+        }
+    }
 
-    void join();
+    /**
+     * Wait for all tasks to start execution and then wait for their completion.
+     */
+    void join() {
+        while (working()) std::this_thread::yield();
+        stop();
+    }
 
-    void add(std::function<void()> task);
+    /**
+     * Add a task to the queue.
+     *
+     * @param task the lambda to execute for the task
+     */
+    void add(std::function<void()> task) {
+        std::unique_lock lk(m);
+        taskQueue.push(task);
+        lk.unlock();
+        cv.notify_one();
+    }
 };
